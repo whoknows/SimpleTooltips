f442e5ffdbdaa0127ace03cd0182c8b7107cc142
libra guest <guillaume.coste@gmail.com>
2012-07-27 05:44:26 -0400

    Canvas imporvement

--- jquery.tooltips.js
+++ jquery.tooltips.js
@@ -38,12 +38,22 @@ function Triangle(html, pos, left, color){
 (function($){
     $.fn.tooltips = function(options){
         function resetPosition(elem, ttip){
-			var pos    = elem.offset();
+			if(!options.multiple){
+                $('.tooltips-wrapper').fadeOut('fast');
+                return true;
+            } else {
+                $('.tooltips-wrapper').css('z-index','998');
+                ttip.css('z-index','999');
+			}
+            
+            var pos    = elem.offset();
 			var left   = pos.left-(options.width/2)+elem.width()/2;
 			var marge  = 10;
+            var isTop  = pos.top - $(document).scrollTop() - 10 < ttip.height();
+            var sign   = isTop ? 1 : -1;
             var canvas = new Triangle(
                                     '<canvas width="30" height="12" class="tooltips-triangle"></canvas>',
-                                    pos.top - $(document).scrollTop() - 10 < ttip.height() ? 'top' : 'bottom',
+                                    isTop ? 'top' : 'bottom',
                                     (options.width/2)-15,
                                     options.color);
 
@@ -52,30 +62,20 @@ function Triangle(html, pos, left, color){
                 left += options.width / 2 - elem.width() / 2 - (pos.left - $(document).scrollLeft()) / 2;
 			}
 
-			ttip.css({
-                'min-height':options.height,
-                'left'  :left,
-                'width' :options.width+'px'
-			});
-
 			ttip.find('canvas').remove();
 			ttip.children().append(canvas.html);
 
-			if(pos.top - $(document).scrollTop() - marge < ttip.height()){
-                ttip.find('canvas').css({'bottom':'100%', 'top':''});
-                ttip.css({'top':pos.top + elem.height()+marge+'px'});
-			} else {
-                ttip.find('canvas').css({'bottom':'', 'top':'100%'});
-                ttip.css({'top':pos.top - ttip.height()-marge+'px'});
-			}
+            ttip.css({
+                'min-height':options.height,
+                'left'      :left,
+                'width'     :options.width+'px',
+                'top':pos.top + (ttip.height()*sign) + (marge*sign) + 'px'
+            }).find('canvas').css({
+                'left'  :canvas.left, 
+                'bottom':isTop ? '' : '100%', 
+                'top'   : isTop ? '100%' : ''
+            });
 
-			ttip.find('canvas').css({'left':canvas.left});
-			if(!options.multiple){
-                $('.tooltips-wrapper').fadeOut('fast');
-			} else {
-                $('.tooltips-wrapper').css('z-index','998');
-                ttip.css('z-index','999');
-			}
 			canvas.elem = ttip.find('canvas')[0];
             canvas.drawMe();
 			ttip.fadeIn('fast');
